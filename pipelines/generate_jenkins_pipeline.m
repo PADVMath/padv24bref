2a49752cc72a89885ec1156c00059e22bd2d5afd
PADVMath <168002500+PADVMath@users.noreply.github.com>
2025-07-31 12:54:54 -0400

    Update generate_jenkins_pipeline.m

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -1,32 +1,42 @@
 % Copyright 2025 The MathWorks, Inc.
 
-function generate_jenkins_pipeline(workspace, projectRelativePath, matlabInstallationLocation, agentLabel, build_folder)
-    arguments
-        workspace = pwd;
-        projectRelativePath = "";
-        matlabInstallationLocation = "matlab_bin_path";
-        agentLabel = "jenkins_agent_label";
-        build_folder = "_build_"
-    end
-    cp = openProject(strjoin({workspace,projectRelativePath} , filesep));
+function generate_jenkins_pipeline2( )
+    workspace = getenv('WORKSPACE');      % Reading Jenkins workspace environment variable
+    supportPackageRoot = getenv('MW_SUPPORT_PACKAGE_ROOT');
+    relativeProjectPath = getenv('MW_RELATIVE_PROJECT_PATH');
+    pipelineGenDirectory = getenv('MW_PIPELINE_GEN_DIRECTORY');
+
+    cp = openProject(strcat(workspace,filesep,string(relativeProjectPath)));
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelPipelines"; # SingleStage, SerialStages, SerialStagesGroupPerTask
     op.PipelineArchitecture = "IndependentModelPipelines";
     op.GeneratorVersion = 2;
-    op.MatlabInstallationLocation = matlabInstallationLocation;
-    op.AgentLabel = agentLabel;
-    op.GeneratedPipelineDirectory = build_folder;
+    op.SupportPackageRoot = supportPackageRoot;
+    op.AgentLabel = "padv_wsl_agents";
+    op.GeneratedPipelineDirectory = pipelineGenDirectory;
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
-    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/PadvReport";
-    op.ProjectRelativePath = projectRelativePath;
+    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
+    op.RelativeProjectPath = relativeProjectPath;
 
+    % We can enhance the vaidation now of the options on the matlab side
+    op.ArtifactServiceMode = 'azure_blob';         % network/jfrog/s3/azure_blob
+    % op.NetworkStoragePath = '<Artifactory network storage path>';
+    % op.ArtifactoryUrl = '<JFrog artifactory url>';
+    % op.ArtifactoryRepoName = '<JFrog artifactory repo name>';
+    % op.S3BucketName = '<AWS S3 bucket name>';
+    % op.S3AwsAcessKeyID = '<AWS S3 access key id>';
+    op.AzContainerName = 'padvblobcontainer';
+    op.RunnerType = "container";        % default/container
+    op.ImageTag = 'slcicd.azurecr.io/slcheck/padv-ci:r2024b_apr25t_ci_spkg20250730';
+    % op.ImageArgs = "<Docker container arguments>";
+    
     % Docker image settings
     op.UseMatlabPlugin = false;
-    % examples: "matlab", "matlab-batch", "xvfb-run -a matlab", "xvfb-run -a matlab-batch"
-    % op.MatlabLaunchCmd = "xvfb-run -a matlab-batch";
-    op.MatlabLaunchCmd = "matlab-batch";
+    % examples: "matlab", "matlab-batch", "xvfb-run -a matlab", "xvfb-run -a matlab-batch"    
+    op.MatlabLaunchCmd = "xvfb-run -a matlab-batch"; 
     op.MatlabStartupOptions = "";
     op.AddBatchStartupOption = false;
+    
     padv.pipeline.generatePipeline(op, "CIPipeline");
 end
