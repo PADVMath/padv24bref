51f14601f48c7021b0850c5a3d7a6c47385c6b48
PADVMath <168002500+PADVMath@users.noreply.github.com>
2025-08-02 17:34:09 -0400

    Update generate_jenkins_pipeline.m

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -9,7 +9,7 @@ function generate_jenkins_pipeline2( )
     cp = openProject(strcat(workspace,filesep,string(relativeProjectPath)));
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelPipelines"; # SingleStage, SerialStages, SerialStagesGroupPerTask
-    op.PipelineArchitecture = "IndependentModelPipelines";
+    op.PipelineArchitecture = "SingleStage";
     op.GeneratorVersion = 2;
     op.SupportPackageRoot = supportPackageRoot;
     op.AgentLabel = "JenkinsAgentMatlabUser";
